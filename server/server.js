740fd41c7cd9b1b13227eaf23d4503addd4e6f42
Shrinath Apake <shreeapke@rediffmail.com>
2020-11-11 17:27:44 +0530

    dev dependency to non-dev

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const path = require('path')
+const publicPath = path.join(__dirname, '..','public')
+const port = process.env.PORT || 3000
+
 const app = express()
-const publicPath = path.join(__dirname, '..', 'public')
 
 app.use(express.static(publicPath))
 
 app.get('*', (req, res)=>{
-    res.sendFile(path.join(publicPath, 'index.html'))
+    res.sendFile(path.join(publicPath,'index.html'))
 })
 
-app.listen(3000,()=>{
+
+
+app.listen(port, ()=>{
     console.log('Server is up')
 })
+
